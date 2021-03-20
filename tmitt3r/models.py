eb9452bfdce013cb1e13233781772beb1703d021
Shunsuke Shimura <mt.cat.yakumo54@gmail.com>
2021-03-20 20:04:34 +0900

    created tm33t model

--- tmitt3r/models.py
+++ tmitt3r/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tm33t(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    posted_time = models.DateTimeField(verbose_name='tm33ted time', auto_now=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
