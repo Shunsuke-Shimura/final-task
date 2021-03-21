6af7b34bbbe4fcad6077d6a7d222a1bc27c89f31
Shunsuke Shimura <mt.cat.yakumo54@gmail.com>
2021-03-21 13:46:28 +0900

    added HomeView test code

--- tmitt3r/tests.py
+++ tmitt3r/tests.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils import timezone
 from .models import Tm33t
+import time
 
 no_csrf_middleware = [
     'django.middleware.security.SecurityMiddleware',
@@ -13,6 +14,7 @@ no_csrf_middleware = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
 class Tm33tModelTests(TestCase):
     def test_str_conversion(self):
         user = User.objects.create_user(username='Tm33tModelTest')
@@ -20,7 +22,36 @@ class Tm33tModelTests(TestCase):
         tm33t = Tm33t.objects.create(poster=user, content=text)
         self.assertEqual(str(tm33t), text[:20])
 
-# Create your tests here.
+
+class HomeViewTests(TestCase):
+    def setUp(self):
+        username = 'HomeViewTestClient'
+        password = 'SamplePassword'
+        self.user = User.objects.create_user(username=username, 
+                                             password=password)
+        self.client.login(username=username, password=password)
+        self.text_list = []
+        for i in range(20):
+            now = timezone.now()
+            text = 'This is HomeViewTest Tm33t text No.{}.'.format(i)
+            Tm33t.objects.create(poster=self.user, content=text)
+            self.text_list.append(text)
+            time.sleep(0.1)
+        self.text_list.reverse()
+    
+    def test_home_view_context(self):
+        """
+        HomeViewによって、contextとしてログインユーザーの
+        直近の10個のTm33tがlatest_tm33t_listとして
+        受け渡される。
+        """
+        res = self.client.get(reverse('tmitt3r:home'))
+        queryset = res.context['latest_tm33t_list']
+        for i in range(10):
+            self.assertEqual(queryset[i].content, self.text_list[i])
+            
+
+
 @override_settings(MIDDLEWARE=no_csrf_middleware)
 class Tm33tViewTests(TestCase):
     def setUp(self):
