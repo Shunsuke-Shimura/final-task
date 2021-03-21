9bacf174df9516cc1124428beb87d6e37ced827e
Shunsuke Shimura <mt.cat.yakumo54@gmail.com>
2021-03-21 13:02:16 +0900

    modified HomeView so that users can see their own latest tm33t.

--- tmitt3r/views.py
+++ tmitt3r/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 from django.utils import timezone
 from django.urls import reverse_lazy
@@ -10,8 +10,16 @@ from .models import Tm33t
 def index(request):
     return render(request, 'tmitt3r/index.html')
 
-class HomeView(LoginRequiredMixin, TemplateView):
+class HomeView(LoginRequiredMixin, ListView):
     template_name = 'tmitt3r/home.html'
+    context_object_name = 'latest_tm33t_list'
+
+    def get_queryset(self):
+        """
+        ログイン中のユーザーの最近の10個のツイートを取得
+        """
+        return Tm33t.objects.filter(poster=self.request.user).order_by('-post_time')[:10]
+
 
 class Tm33tView(LoginRequiredMixin, CreateView):
     model = Tm33t
