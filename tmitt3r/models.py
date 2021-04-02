8d9cdf3fa7cb51c86bbec01292eea9e6fcc9a6d8
Shunsuke Shimura <mt.cat.yakumo54@gmail.com>
2021-04-02 10:21:11 +0900

    reset database and migrations

--- tmitt3r/models.py
+++ tmitt3r/models.py
@@ -5,6 +5,8 @@ class Tm33t(models.Model):
     poster = models.ForeignKey(User, related_name='tm33ts', on_delete=models.CASCADE)
     post_time = models.DateTimeField(verbose_name='tm33t time', auto_now=True)
     content = models.TextField()
+    users_liked = models.ManyToManyField(User, related_name='tm33ts_liked')
 
     def __str__(self):
-        return self.content[:20]
+        length = (len(self.content) - 1) if (len(self.content) < 20) else 18
+        return repr(self.content[:length])
