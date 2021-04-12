3f7b851eab158126539adaad0d35ad23f35406ee
Shunsuke Shimura <mt.cat.yakumo54@gmail.com>
2021-04-12 23:45:52 +0900

    created Retm33t model

--- tmitt3r/models.py
+++ tmitt3r/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Tm33t(models.Model):
     poster = models.ForeignKey(User, related_name='tm33ts', on_delete=models.CASCADE)
     post_time = models.DateTimeField(verbose_name='tm33t time', auto_now=True)
-    content = models.TextField()
+    content = models.TextField(null=True)
     users_liked = models.ManyToManyField(User, related_name='tm33ts_liked')
 
     def __str__(self):
@@ -19,6 +19,13 @@ class Tm33t(models.Model):
     def is_reply(self):
         return hasattr(self, 'reply')
 
+    def is_retm33t(self):
+        return hasattr(self, 'retm33t')
+
 
 class Reply(Tm33t):
     related_tm33t = models.ForeignKey(Tm33t, related_name="replies", on_delete=models.CASCADE)
+
+
+class Retm33t(Tm33t):
+    tm33t_retm33ted = models.ForeignKey(Tm33t, related_name="retm33ts", on_delete=models.CASCADE)
