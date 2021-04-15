dc7064ba4fc1970153622ee7241a54244ee1a802
Shunsuke Shimura <mt.cat.yakumo54@gmail.com>
2021-04-15 20:31:14 +0900

    Implemented like_state custom template tag

--- tmitt3r/templatetags/tm33t_state.py
+++ tmitt3r/templatetags/tm33t_state.py
@@ -8,3 +8,10 @@ def retm33t_state(tm33t, user):
     if tm33t.has_been_retm33ted(user):
         return 'retm33ted'
     return 'unretm33ted'
+
+@register.simple_tag
+def like_state(tm33t, user):
+    """Returns tm33t's like state as string (like or unlike)"""
+    if tm33t.has_been_liked(user):
+        return 'like'
+    return 'unlike'
